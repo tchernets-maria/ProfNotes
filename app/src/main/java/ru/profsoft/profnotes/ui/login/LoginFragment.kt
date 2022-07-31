d8bd2be328977a9c87529cc5b38bf14297fff0b0
tchernets-maria <tchernatsmaria@yandex.ru>
2022-08-01 03:15:47 +0400

    Add HomeFragment

--- app/src/main/java/ru/profsoft/profnotes/ui/login/LoginFragment.kt
+++ app/src/main/java/ru/profsoft/profnotes/ui/login/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import ru.profsoft.profnotes.R
 import ru.profsoft.profnotes.core.spanString
 import ru.profsoft.profnotes.databinding.FragmentLoginBinding
@@ -33,12 +35,20 @@ class LoginFragment : Fragment() {
                 color = requireContext().getColor(R.color.green_85)
             )
         }
+
         binding.tvRecoverPassword.apply {
             text = text.spanString(
                 startIndex = 24,
                 endIndex = 43,
                 color = requireContext().getColor(R.color.yellow_16)
             )
+        }
+
+        binding.btnLogin.apply {
+            findViewById<Button>(R.id.btnLogin).isEnabled = true
+            setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_navigation_home)
+            }
         }
     }
 
