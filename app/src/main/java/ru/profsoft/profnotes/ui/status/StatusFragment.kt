e3fafc040248b5d6f33b3fddf31c270fc52eec47
tchernets-maria <tchernatsmaria@yandex.ru>
2022-08-31 12:55:01 +0400

    add status fragment

--- app/src/main/java/ru/profsoft/profnotes/ui/status/StatusFragment.kt
+++ app/src/main/java/ru/profsoft/profnotes/ui/status/StatusFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import ru.profsoft.profnotes.R
 
+@AndroidEntryPoint
 class StatusFragment : Fragment() {
 
     override fun onCreateView(
