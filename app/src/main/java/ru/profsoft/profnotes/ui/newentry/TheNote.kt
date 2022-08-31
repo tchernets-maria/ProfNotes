5c7324d48be1f4adac919df8dc19922dc2487d5f
tchernets-maria <tchernatsmaria@yandex.ru>
2022-08-31 13:03:07 +0400

    adding a snippet to create a new note

--- app/src/main/java/ru/profsoft/profnotes/ui/newentry/TheNote.kt
+++ app/src/main/java/ru/profsoft/profnotes/ui/newentry/TheNote.kt
@@ -1,10 +1,11 @@
-package ru.profsoft.profnotes
+package ru.profsoft.profnotes.ui.newentry
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.profsoft.profnotes.R
 
 class TheNote : Fragment() {
 
